146cbcca58dde9e9ee81a539962305b80428ab77
Lorena <lllorenaz123@gmail.com>
2018-11-22 23:38:40 -0700

    phase3

--- prj2/script/phase3/phas3.py
+++ prj2/script/phase3/phas3.py
@@ -1,1 +1,79 @@
 from bsddb3 import db
+import sys
+import re
+
+db_terms = None
+db_ads = None
+db_dates = None
+db_prices = None
+
+cur_terms = None
+cur_ads = None
+cur_dates = None
+cur_prices = None
+def createDB():
+	global db_terms, db_ads, db_dates, db_prices
+	global cur_terms, cur_ads, cur_dates, cur_prices
+
+	db_terms = db.DB()
+	db_terms.set_flags(db.DB_DUP) #declare duplicates allowed before you create the database
+	db_terms.open("te.idx", None, db.DB_BTREE, db.DB_CREATE)
+
+	db_ads = db.DB()
+	db_ads.set_flags(db.DB_DUP) #declare duplicates allowed before you create the database
+	db_ads.open("ad.idx", None, db.DB_HASH, db.DB_CREATE)
+
+	db_dates = db.DB()
+	db_dates.set_flags(db.DB_DUP) #declare duplicates allowed before you create the database
+	db_dates.open("da.idx", None, db.DB_BTREE, db.DB_CREATE)
+
+	db_prices = db.DB()
+	db_prices.set_flags(db.DB_DUP) #declare duplicates allowed before you create the database
+	db_prices.open("pr.idx", None, db.DB_BTREE, db.DB_CREATE)
+
+def from_file():
+	
+
+
+def from_input():
+
+def main():
+	global db_terms, db_ads, db_dates, db_prices
+	global cur_terms, cur_ads, cur_dates, cur_prices
+
+	createDB()
+
+	decision = str(input("1. Read from file.\n2. Read from input\n3. Qui\n Enter:\t"))
+
+	while decision != '3':
+		if decision == '1':
+			from_file()
+			input_filename = str(input("Please enter the inout file name: "))
+			output_filename = str(input("Please enter the output file name: "))
+			input_file = open(input_filename,"r")
+			output_file = open(output_filename,"w")
+		
+		elif decision == '2':
+			from_input()
+		
+		decision = str(input("1. Read from file.\n2. Read from input\n3. Qui\n Enter:\t"))
+
+
+
+	cur_prices.close()
+	db_prices.close()
+
+	cur_dates.close()
+	db_dates.close()
+
+	cur_ads.close()
+	db_ads.close()
+
+	cur_terms.close()
+	db_terms.close()
+
+
+
+
+if __name__ == "__main__":
+    main()
