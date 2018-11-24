98068c19de9065dbb723ea4c6eb17128a08326b4
Lorena <lllorenaz123@gmail.com>
2018-11-24 09:47:58 -0700

    re

--- prj2/script/phase3/phas3.py
+++ prj2/script/phase3/phas3.py
@@ -32,13 +32,22 @@ def createDB():
 	db_prices.open("pr.idx", None, db.DB_BTREE, db.DB_CREATE)
 
 def search(query,type):
+	print(query)
+	z = re.findall(r"[.\s]*(date[>=<\s]+\d\d\d\d[\/]\d\d[\/]\d\d)[\s]*",query)
+	if z:
+		print(z.groups())
+
+	#z = re.match("[.\s]*date[\s]*[<>=]?[\s]*\d{4}+[\/]\d{2}+[\/]{2}",query)
+	#z = re.match("[.\s]*date[>=<]+\d{4}\d{2}\d{2}",query)
+	
+'''
 	q = ''
 	flag = 0 #use to determine whether this is a longterm condtion(ie. date <= 2018/11/05)
 	count = 0
 	whitespce = [' ','\r','\t','\f','\v']
 	keywords = ['date','cat','price','location']
-
 	keychar = ['=','<','>','<=','>=']
+
 	for i in range(len(query)):
 		if query[i] in whitespce and query[i-1] not in whitespce:
 			q += '産'
@@ -49,7 +58,7 @@ def search(query,type):
 	
 	conditions = q.split('産')
 	result = {}
-	print(conditions)
+	#print(conditions)
 	
 	querys=[]
 	for i in range(len(conditions)):
@@ -62,10 +71,33 @@ def search(query,type):
 		elif conditions[i] not in keywords:
 			querys.append(conditions[i])
 
+	#print(querys)
+
+	for each in conditions:
+		if each == "output=full":
+			count -= 1
+			continue
+		elif each == "output=key":
+			count -= 1
+			continue
+
+		elif re.match('\Adate',each):
+			date(each[4:])
+		elif re.match('\Aprice',each):
+			price(each[4:])
+		elif re.match('\Alocation',each):
+			location(each[9:])
+		else:
+
+		elif re.match('[\s]+date[>=<\s]+\d{4}+[\/]+\d{2}+[\/]+\d{2}+[\s]+'):
+			print()
+'''
+
+
+	
 
 
 
-	print(querys)
 
 
 
@@ -75,22 +107,7 @@ def search(query,type):
 
 
 
-
-
-
-
-
-
-
-
-
-#	for i in conditions:
-#		if each == "output=full":
- #           count -= 1
-#            continue
-#        elif each == "output=key":
-#            count -= 1
-#            continue
+	
 
         
 
