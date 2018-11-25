ac1845badfc9775314db566534ab320146271245
TianyuBai-Viola <44730845+TianyuBai-Viola@users.noreply.github.com>
2018-11-24 21:39:07 -0700

    Update phase1.py

--- prj2/phase1.py
+++ prj2/phase1.py
@@ -78,8 +78,9 @@ def main():
                 pr = re.search(r'<price>(.*?)</price>',line)
             except AttributeError:
                 pr = None
-            s = " "*(12-len(pr.group(1)))
-            new3.write(s+pr.group(1)+":"+aid.group(1)+","+cat.group(1)+","+loc.group(1)+"\n")
+            #s = " "*(12-len(pr.group(1)))
+            #new3.write(s+pr.group(1)+":"+aid.group(1)+","+cat.group(1)+","+loc.group(1)+"\n")
+            new3.write(pr.group(1)+":"+aid.group(1)+","+cat.group(1)+","+loc.group(1)+"\n")
             
             
             
